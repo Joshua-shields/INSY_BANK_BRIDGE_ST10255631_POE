89adf85b82d41f3c95f05e6fd3d70d0a90464265
kashc <kashc@THE_BEAST>
2025-11-06 14:16:26 +0200

    commented the checkuser.js, added jheaders and footers

--- check_users.js
+++ check_users.js
@@ -1,3 +1,5 @@
+
+//--------------------------- start of file ----------------------------//
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -10,10 +12,17 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 mongoose.connect('mongodb://localhost:27017/bank_bridge').then(async () => {
+  // Fetch all user documents from the database
   const users = await User.find({});
+  // Log the users 
   console.log('Registered users:', users);
+  // Exit the process successfully
   process.exit(0);
 }).catch(err => {
+  // Log any connection or query errors
   console.error('Error:', err);
+  // Exit the process with an error code
   process.exit(1);
 });
+
+//---------------------------------- end of file ---------------------------//
