9fc93709cb4628a6d883dfc00eb7a7605891c8ee
Joshua Dean Shields <shields.joshua23@gmail.com>
2025-11-06 12:32:50 +0200

    Fix CSRF token authentication for Windows users by updating API to use HTTPS

--- customer/src/utils/api.js
+++ customer/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Base URL for the API
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://localhost:3000';
 
 // Create axios instance with default config
 const api = axios.create({
